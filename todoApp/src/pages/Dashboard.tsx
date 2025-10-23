398685cb57cb9cbaf92186b4bde787d7f20b219f
t-masuda-ctof <taka1dptx@gmail.com>
2025-10-23 23:41:35 +0900

    TODO一覧作成

--- todoApp/src/pages/Dashboard.tsx
+++ todoApp/src/pages/Dashboard.tsx
@@ -1,43 +1,89 @@
-import { Box, Typography, Container, Paper } from "@mui/material";
+import { Box, Typography, Container, Paper, Snackbar, Alert } from "@mui/material";
+import { useEffect, useState } from "react";
+import Todo from "../components/Todo";
 
 export default function Dashboard() {
-    return (
-        <Container
-            maxWidth={false}
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+
+  useEffect(() => {
+    setOpenSnackbar(true);
+  }, []);
+
+  const todos = [
+    { date: "2025-10-23", title: "Reactの型定義を確認する", author: "たくみ" },
+    { date: "2025-10-23", title: "Todo一覧の表示を実装する", author: "たかと" },
+    { date: "2025-10-25", title: "API連携の設計を考える", author: "たくみ？" },
+  ];
+
+  return (
+    <Container
+      maxWidth={false}
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "#b5b4b4ff",
+        padding: 4,
+        boxSizing: "border-box",
+      }}
+    >
+      <Paper
+        elevation={6}
+        sx={{
+          width: "100%",
+          height: "100%",
+          backgroundColor: "#fff",
+          padding: 4,
+          boxSizing: "border-box",
+        }}
+      >
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+          <Typography variant="h4" align="center">
+            ダッシュボード
+          </Typography>
+
+          <Typography variant="h6" align="center">
+            TODO一覧
+          </Typography>
+
+          <Box
             sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                width: "100vw",
-                height: "100vh",
-                backgroundColor: "#6e6c6cff", // ログイン画面と同じ背景色
+              display: "flex",
+              justifyContent: "space-between",
+              fontWeight: "bold",
+              px: 2,
+              py: 1,
+              backgroundColor: "#eeeeee",
+              borderRadius: 1,
             }}
-        >
-            <Paper
-                elevation={6}
-                sx={{
-                    p: 4,
-                    maxWidth: 500,
-                    width: "100%",
-                    backgroundColor: "#424242", // 少し濃いダークグレー
-                    color: "#fff",
-                }}
-            >
-                <Box
-                    sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                    }}
-                >
-                    <Typography variant="h4" sx={{ mb: 2 }}>
-                        ダッシュボード
-                    </Typography>
-                    <Typography variant="body1">
-                        ログインに成功しました 🎉
-                    </Typography>
-                </Box>
-            </Paper>
-        </Container>
-    );
+          >
+            <Typography sx={{ width: "30%" }}>日付</Typography>
+            <Typography sx={{ width: "40%", textAlign: "center" }}>タイトル</Typography>
+            <Typography sx={{ width: "30%", textAlign: "right" }}>作成者</Typography>
+          </Box>
+
+          {/* TODO一覧 */}
+          {todos.map((todo, index) => (
+            <Todo
+              key={index}
+              date={todo.date}
+              title={todo.title}
+              author={todo.author}
+            />
+          ))}
+        </Box>
+      </Paper>
+
+      {/* ポップアップ通知 */}
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={3000}
+        onClose={() => setOpenSnackbar(false)}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      >
+        <Alert severity="success" sx={{ width: "100%" }}>
+          ログインに成功しました 🎉
+        </Alert>
+      </Snackbar>
+    </Container>
+  );
 }
