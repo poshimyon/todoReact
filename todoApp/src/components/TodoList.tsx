922816f9af4664180fddd1f6579d50951dae8c72
takumi.iihoshi <takumi75.poshi@gmail.com>
2025-10-28 22:59:02 +0900

    [WIP]コンポーネント分割

---
+++ todoApp/src/components/TodoList.tsx
@@ -0,0 +1,28 @@
+import Todo from "./Todo";
+import { Box } from "@mui/material";
+
+type TodoType = {
+    id: string;
+    title: string;
+    author: string;
+    date: string;
+};
+
+type Props = {
+    todos: TodoType[];
+};
+
+export default function TodoList({ todos }: Props) {
+    return (
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 0 }}>
+            {todos.map((todo) => (
+                <Todo
+                    key={todo.id}
+                    date={todo.date}
+                    title={todo.title}
+                    author={todo.author}
+                />
+            ))}
+        </Box>
+    );
+}
--- todoApp/src/pages/Dashboard.tsx
+++ todoApp/src/pages/Dashboard.tsx
@@ -1,105 +1,75 @@
-import { Box, Typography, Container, Paper, Snackbar, Alert } from "@mui/material";
+import { Box, Container, Paper } from "@mui/material";
 import { useEffect, useState } from "react";
-import Todo from "../components/Todo";
 import axios from "axios";
+import DashboardHeader from "../components/DashboardHeader";
+import TodoList from "../components/TodoList";
+import LoginSnackbar from "../components/LoginSnackbar";
 
 type TodoType = {
-  id: string;
-  title: string;
-  author: string;
-  date: string;
+    id: string;
+    title: string;
+    author: string;
+    date: string;
 };
 
 export default function Dashboard() {
-  const [openSnackbar, setOpenSnackbar] = useState(false);
-  const [todos, setTodos] = useState<TodoType[]>([]);
+    const [openSnackbar, setOpenSnackbar] = useState(false);
+    const [todos, setTodos] = useState<TodoType[]>([]);
 
-  useEffect(() => {
-    const fetchTodos = async () => {
-      try {
-        const res = await axios.get<TodoType[]>("http://localhost:3000/todos");
+    useEffect(() => {
+        const fetchTodos = async () => {
+            try {
+                const res = await axios.get<TodoType[]>(
+                    "http://localhost:3000/todos"
+                );
 
-        // id順に昇順ソート
-        const sorted = res.data.sort((a, b) => a.id.localeCompare(b.id));
+                // id順に昇順ソート
+                const sorted = res.data.sort((a, b) =>
+                    a.id.localeCompare(b.id)
+                );
 
-        setTodos(sorted);
-      } catch (err) {
-        console.error("❌ Failed to fetch todos", err);
-      }
-    };
-    fetchTodos();
-    setOpenSnackbar(true);
-  }, []);
+                setTodos(sorted);
+            } catch (err) {
+                console.error("❌ Failed to fetch todos", err);
+            }
+        };
+        fetchTodos();
+        setOpenSnackbar(true);
+    }, []);
 
-  return (
-    <Container
-      maxWidth={false}
-      sx={{
-        width: "100vw",
-        height: "100vh",
-        backgroundColor: "#b5b4b4ff",
-        padding: 4,
-        boxSizing: "border-box",
-      }}
-    >
-      <Paper
-        elevation={6}
-        sx={{
-          width: "100%",
-          height: "100%",
-          backgroundColor: "#fff",
-          padding: 4,
-          boxSizing: "border-box",
-        }}
-      >
-        <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-          <Typography variant="h4" align="center">
-            ダッシュボード
-          </Typography>
-
-          <Typography variant="h6" align="center">
-            TODO一覧
-          </Typography>
-
-          <Box
+    return (
+        <Container
+            maxWidth={false}
             sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              fontWeight: "bold",
-              px: 2,
-              py: 1,
-              backgroundColor: "#eeeeee",
-              borderRadius: 1,
+                width: "100vw",
+                height: "100vh",
+                backgroundColor: "#b5b4b4ff",
+                padding: 4,
+                boxSizing: "border-box",
             }}
-          >
-            <Typography sx={{ width: "30%" }}>日付</Typography>
-            <Typography sx={{ width: "40%", textAlign: "center" }}>タイトル</Typography>
-            <Typography sx={{ width: "30%", textAlign: "right" }}>作成者</Typography>
-          </Box>
+        >
+            <Paper
+                elevation={6}
+                sx={{
+                    width: "100%",
+                    height: "100%",
+                    backgroundColor: "#fff",
+                    padding: 4,
+                    boxSizing: "border-box",
+                }}
+            >
+                <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+                    <DashboardHeader />
 
-          {/* TODO一覧 */}
-          {todos.map((todo) => (
-            <Todo
-              key={todo.id}
-              date={todo.date}
-              title={todo.title}
-              author={todo.author}
+                    {/* TODO一覧 */}
+                    <TodoList todos={todos} />
+                </Box>
+            </Paper>
+
+            <LoginSnackbar
+                open={openSnackbar}
+                onClose={() => setOpenSnackbar(false)}
             />
-          ))}
-        </Box>
-      </Paper>
-
-      {/* ポップアップ通知 */}
-      <Snackbar
-        open={openSnackbar}
-        autoHideDuration={3000}
-        onClose={() => setOpenSnackbar(false)}
-        anchorOrigin={{ vertical: "top", horizontal: "center" }}
-      >
-        <Alert severity="success" sx={{ width: "100%" }}>
-          ログインに成功しました 🎉
-        </Alert>
-      </Snackbar>
-    </Container>
-  );
+        </Container>
+    );
 }
